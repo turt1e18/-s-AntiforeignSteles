5248f78c5b676ccefd0619759276a6f97879d652
turt1e18 <pk0820@cu.ac.kr>
2025-04-02 23:52:33 +0900

    0402

--- 5430.py
+++ 5430.py
@@ -37,3 +37,44 @@ for i in range(cmdList):
         result.append(f"[{','.join(map(str, dq))}]")
 
 print("\n".join(result))
+
+from sys import stdin
+from collections import deque
+
+cmdList = int(stdin.readline().rstrip())
+result = []
+
+for _ in range(cmdList):
+    isError = False
+    isReverse = False
+
+    cmd = stdin.readline().rstrip()
+    n = int(stdin.readline().rstrip())
+    arr = stdin.readline().strip()
+
+    if n == 0:
+        dq = deque()
+    else:
+        listArr = list(map(int, arr.strip()[1:-1].split(",")))
+        dq = deque(listArr)
+
+    for c in cmd:
+        if c == "R":
+            isReverse = not isReverse
+        else:
+            if not dq:
+                isError = True
+                break
+            if isReverse:
+                dq.pop()
+            else:
+                dq.popleft()
+
+    if isError:
+        result.append("error")
+    else:
+        if isReverse:
+            dq.reverse()
+        result.append(f"[{','.join(map(str, dq))}]")
+
+print("\n".join(result))
