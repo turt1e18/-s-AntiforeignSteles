8a0c15bb7e52ab60c2ac2f6fff202fe73ff81e1e
turt1e18 <pk0820@cu.ac.kr>
2025-04-02 23:49:34 +0900

    0402

---
+++ 5430.py
@@ -0,0 +1,39 @@
+from sys import stdin
+from collections import deque
+
+cmdList = int(stdin.readline().rstrip())
+result = []
+for i in range(cmdList):
+    isError = False
+    isReverse = False
+
+    cmd = list(stdin.readline().rstrip())
+    n = int(stdin.readline().rstrip())
+    arr = stdin.readline().strip()
+
+    if n == 0:
+        isError = True
+    else:
+        listArr = list(map(int, arr.strip()[1:-1].split(",")))
+        dq = deque(listArr)
+        
+    for i in cmd:
+        if i == "R":
+            isReverse = not isReverse
+        else:
+            if isError : break
+            if not dq: 
+                isError = True 
+                break
+            
+            if isReverse: dq.pop()
+            else: dq.popleft()
+
+    if isError:
+        result.append("error")
+    else:
+        if isReverse: 
+            dq.reverse()
+        result.append(f"[{','.join(map(str, dq))}]")
+
+print("\n".join(result))
